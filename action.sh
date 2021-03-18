2e97f5bad827425c98159c976dc7ac929757000a
Dan Uhl <duhl@wayfair.com>
2021-03-18 17:02:02 -0400

    adds app and deploy logic

--- action.sh
+++ action.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
-echo 'I am dan'
+echo 'Its working'
 echo $BUILD_DIR
 echo $TARGET_DIR
+
+cd $GITHUB_WORKSPACE
+yarn
+yarn build
+mv $BUILD_DIR $TARGET_DIR
+git add $TARGET_DIR
+git commit -m "Deploy 🦾"
+git push
---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div
+      style={{
+        maxWidth: '700px',
+        textAlign: 'center',
+        margin: '2em auto',
+        color: '#f50'
+      }}
+    >
+      <h1>You&apos;re doing great.</h1>
+    </div>
+  );
+};
+
+export default App;
