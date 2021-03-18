30db61fa49f7c662fab151edfcb61a939d5055eb
Dan Uhl <duhl@wayfair.com>
2021-03-18 17:04:44 -0400

    adds configs

--- action.sh
+++ action.sh
@@ -3,6 +3,9 @@ echo 'Its working'
 echo $BUILD_DIR
 echo $TARGET_DIR
 
+git config user.email $GITHUB_ACTOR@bots.github.com
+git config user.name $GITHUB_ACTOR
+
 cd $GITHUB_WORKSPACE
 yarn
 yarn build
